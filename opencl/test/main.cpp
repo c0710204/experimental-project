324665b8346cf5427c40854d4689ae4100fb7535
Xiang Gu <c0710204@gmail.com>
2016-03-18 20:51:45 -0700

    cl-header

---
+++ opencl/test/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <unistd.h>
+#include <iostream>
+#include <cstdio>
+#include "OpenCL-Headers/cl.h"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
